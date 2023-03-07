cef190c74aa5af0e33648cf0b7ad4d5622efcccf
TippoBT <61446612+TippoBT@users.noreply.github.com>
2023-03-07 18:53:32 +1000

    Update aimee.py

--- aimee.py
+++ aimee.py
@@ -1,1 +1,15 @@
+# This is a copy-paste from the Discord Red documentation on how to start this file, I have no clue on Python atm so naming will be screwed up
 
+from redbot.core import commands
+
+class AIMEE(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
